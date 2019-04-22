02a54bb3b9aea4a972e193b1f5c885b83c727c1e
Dmitralt <47825098+Dmitralt@users.noreply.github.com>
2019-04-22 15:46:03 +0300

    Add files via upload

---
+++ finding_differences.js
@@ -0,0 +1,43 @@
+var fs = require("fs");
+var json1 = fs.readFileSync("first.json");
+var json2 = fs.readFileSync("second.json");
+var jsonContent1 = JSON.parse(json1);
+var jsonContent2 = JSON.parse(json2);
+function Printjson(name, value) { console.log("\t\t" + name + ":" + value); };
+for (var key1 in jsonContent1) {
+
+    for (var key2 in jsonContent2) {
+        if (key1 == key2)//ищем ключи что совпадают
+        {
+            if ((JSON.stringify(jsonContent1[key1]) == JSON.stringify(jsonContent2[key2])) == false)//Если в Объектах есть различия, печатаем ключ
+            {
+                console.log(key1);
+            }
+            for (var variable in jsonContent1[key1]) {
+
+                if ((jsonContent1[key1][variable] == jsonContent2[key1][variable]) == false) {
+                    if (Array.isArray(jsonContent1[key1][variable]) == true) { //если элемент массив, а не переменная, то отдельная проверка
+                        if ((JSON.stringify(jsonContent1[key1][variable]) == JSON.stringify(jsonContent2[key2][variable])) == false) {
+                            console.log("\t"+variable);
+                            Printjson("json1", jsonContent1[key1][variable]);
+                            Printjson("json2", jsonContent2[key2][variable]);
+                        }
+                    }
+                    else {
+                        console.log("\t"+variable);
+                        Printjson("json1", jsonContent1[key1][variable]);
+                        Printjson("json2", jsonContent2[key2][variable]);
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
